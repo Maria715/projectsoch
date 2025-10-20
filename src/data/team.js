1d0c618ebffdba335a1dcf08d9eeb41a848bb5b6
Sharifa <66180927+MuhammadSaqib786@users.noreply.github.com>
2025-10-20 18:15:51 +0200

    Added Team Images and Json File

---
+++ src/data/team.js
@@ -0,0 +1,112 @@
+const volunteers = [
+  {
+    id: 1,
+    name: "Maria Akhtar",
+    role: "Founder & CEO",
+    image: `${process.env.PUBLIC_URL}/images/team/maria.png`
+  },
+  {
+    id: 2,
+    name: "Muhammad Saqib",
+    role: "Co-Founder & Head of Technology",
+    image: `${process.env.PUBLIC_URL}/images/team/Muhammad Saqib – Head of Technology.png`
+  },
+  {
+    id: 3,
+    name: "Muhammad Arslan Bosan",
+    role: "Co-Founder & Head of Ops",
+    image: `${process.env.PUBLIC_URL}/images/team/arslan.png`
+  },
+  {
+    id: 4,
+    name: "Ahmed Masood",
+    role: "Communication Manager - Germany Chapter",
+    image: `${process.env.PUBLIC_URL}/images/team/Ahmed Masood.jpeg`
+  },
+  {
+    id: 5,
+    name: "Ameema Waheed",
+    role: "Social Media Team",
+    image: `${process.env.PUBLIC_URL}/images/team/Ameema.jpeg`
+  },
+  {
+    id: 6,
+    name: "Amina Arshad",
+    role: "Front-end",
+    image: `${process.env.PUBLIC_URL}/images/team/Amina Arshad.jpg`
+  },
+  {
+    id: 7,
+    name: "Ayesha Iqbal",
+    role: "Front-end",
+    image: `${process.env.PUBLIC_URL}/images/team/Ayesha Iqbal.jpeg`
+  },
+  {
+    id: 8,
+    name: "Ayesha Shahzaib",
+    role: "Social Media",
+    image: `${process.env.PUBLIC_URL}/images/team/Ayesha Shahzaib_.jpg`
+  },
+  {
+    id: 9,
+    name: "Fariha Ahson",
+    role: "Spokesperson",
+    image: `${process.env.PUBLIC_URL}/images/team/Fariha Ahson.jpg`
+  },
+  {
+    id: 10,
+    name: "Fozia Bibi",
+    role: "Social Media Team",
+    image: `${process.env.PUBLIC_URL}/images/team/fozia bibi Social Media.jpeg`
+  },
+  {
+    id: 11,
+    name: "Ghulam Muhammad Khan",
+    role: "Front-end",
+    image: `${process.env.PUBLIC_URL}/images/team/Ghulam Muhammad Khan.png`
+  },
+  {
+    id: 12,
+    name: "Hamna Zahid",
+    role: "Front-end",
+    image: `${process.env.PUBLIC_URL}/images/team/Hamna Zahid - Frontend Developer.jpg`
+  },
+  {
+    id: 13,
+    name: "Hamza Ahmed",
+    role: "Front-end",
+    image: `${process.env.PUBLIC_URL}/images/team/hamza.png`
+  },
+  {
+    id: 14,
+    name: "Muhammad Ghayoor Ali",
+    role: "Full Stack Developer",
+    image: `${process.env.PUBLIC_URL}/images/team/Muhammad Ghayoor Ali- Full Stack Developer.jpg`
+  },
+  {
+    id: 15,
+    name: "Muhammad Hasnain",
+    role: "Outreach & Cases Verification Team",
+    image: `${process.env.PUBLIC_URL}/images/team/Muhammad Hasnain_.jpg`
+  },
+  {
+    id: 16,
+    name: "Muhammad Hussain",
+    role: "Graphic Designer",
+    image: `${process.env.PUBLIC_URL}/images/team/Muhammad Hussain – Graphic Designer.jpeg`
+  },
+  {
+    id: 17,
+    name: "Nasir Zaman",
+    role: "Backend Developer",
+    image: `${process.env.PUBLIC_URL}/images/team/Nasir Zaman - Backend Lead.jpg`
+  },
+  {
+    id: 18,
+    name: "Sayyam Sabir",
+    role: "Product Owner",
+    image: `${process.env.PUBLIC_URL}/images/team/Sayam Sabir - Product Owner.jpg`
+  }
+];
+
+export default volunteers;
